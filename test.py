c47bd31926cde7786222cf0f68489f028055d53e
2runo <2runo04@gmail.com>
2020-12-15 03:35:05 +0900

    update: test cases

--- test.py
+++ test.py
@@ -2,15 +2,13 @@ from ic import IC
 
 if __name__ == "__main__":
     ic = IC()
-    print(ic('민물고기'))
-    print(ic('벽돌집'))
-    print(ic('살얼음'))
-    print(ic('나들이옷'))
-    print(ic('반갑습니다'))
-    print(ic('나는 푸른'))
-    print(ic('하늘을 보았다'))
-    print(ic('나는'))
-    print(ic('푸른'))
-    print(ic('하늘을'))
-    print(ic('보았다'))
-    print(ic('인생은 짧지만 예술은 길다'))
+    print(ic('민물고기'))  # 민물 + 고기
+    print(ic('벽돌집'))  # 벽돌 + 집
+    print(ic('살얼음'))  # 살 + 얼음
+    print(ic('나들이옷'))  # 나들이 + 옷
+    print(ic('반갑습니다'))  # 반갑습니다
+    print(ic('하늘을 보았다'))  # 하늘을 + 보았다
+    print(ic('하늘을'))  # 하늘 + 을
+    print(ic('보았다'))  # 보았다
+    print(ic('인생은 짧지만 예술은 길다'))  # 인생은 짧지만 + 예술은 길다
+    print(ic('나는 밥을 먹으러 식당에 간다'))  # 나는 밥을 먹으러 + 식당에 간다
